cdeb522bcbf4e832e0034bc77af8f3c549dafbd5
Matheus Albuquerque <mvca@cin.ufpe.br>
2021-07-01 00:21:03 -0300

    randomly respawning the gk

--- envs/rc_gym/vss/env_selfplay_atk_vs_gk/vss_selfplay_atk_vs_gk.py
+++ envs/rc_gym/vss/env_selfplay_atk_vs_gk/vss_selfplay_atk_vs_gk.py
@@ -442,7 +442,7 @@ class VSSSelfplayAtkGk(VSSBaseEnv):
                     self.reward_shaping_total['distance_own_goal_bar_gk'] += w_distance * dist_robot_own_goal_bar_gk
                     self.reward_shaping_total['defense_gk'] += ball_defense_reward * w_defense
                     self.reward_shaping_total['ball_leave_area_gk'] += w_ball_leave_area * ball_leave_area_reward
-                    self.reward_shaping_total['gk_leave_area'] = gk_leave_area_reward
+                    self.reward_shaping_total['gk_leave_area'] += gk_leave_area_reward
 
                 # Attacker Reward
                 # Calculate ball potential Attacker
@@ -494,12 +494,15 @@ class VSSSelfplayAtkGk(VSSBaseEnv):
         pos_frame.ball.v_x = 0.
         pos_frame.ball.v_y = 0.
 
-        pos_frame.robots_blue[0] = Robot(x=-field_half_length + 0.05,
-                                         y=0.0,
+        x_gk = random.uniform(-field_half_length + 0.05,
+                               pos_frame.ball.x)
+        y_gk = y()
+        pos_frame.robots_blue[0] = Robot(x=x_gk,
+                                         y=y_gk,
                                          theta=0)
         agents = []
-        agents.append(Robot(x=-field_half_length + 0.05, y=0.0, theta=0))
-
+        agents.append(Robot(x=x_gk, y=y_gk, theta=0))
+        
         for i in range(1, self.n_robots_blue):
             pos_frame.robots_blue[i] = Robot(x=x(), y=y(), theta=theta())
             agents.append(pos_frame.robots_blue[i])
