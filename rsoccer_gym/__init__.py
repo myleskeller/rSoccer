1ce3a0a83a3081a587da5e216223600b0a66feed
Breno Cavalcanti <breno.uc.cavalcanti@gmail.com>
2021-09-08 17:39:05 -0300

    vss_env_5v5: crated and registered the env 5v5

--- rsoccer_gym/__init__.py
+++ rsoccer_gym/__init__.py
@@ -4,7 +4,10 @@ register(id='VSS-v0',
          entry_point='rsoccer_gym.vss.env_vss:VSSEnv',
          max_episode_steps=1200
          )
-
+register(id='VSS5V5-v0',
+         entry_point='rsoccer_gym.vss.env_vss:VSS5v5Env',
+         max_episode_steps=1200
+        )
 register(id='VSSMA-v0',
          entry_point='rsoccer_gym.vss.env_ma:VSSMAEnv',
          max_episode_steps=1200
--- rsoccer_gym/vss/env_vss/__init__.py
+++ rsoccer_gym/vss/env_vss/__init__.py
@@ -1,2 +1,3 @@
 from rsoccer_gym.vss.env_vss.vss_gym import VSSEnv
 from rsoccer_gym.vss.env_vss.vss_gym_fira import VSSFIRAEnv
+from rsoccer_gym.vss.env_vss.vss_gym_5v5 import VSS5v5Env
