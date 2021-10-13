d62cd4437a72266e43f93656bdbcde5333a2219b
matheusalb <mvca@cin.ufpe.br>
2021-10-13 01:10:05 -0300

    created env atk vs gk

--- rsoccer_gym/__init__.py
+++ rsoccer_gym/__init__.py
@@ -25,6 +25,11 @@ register(id='VSSFIRA-v0',
          max_episode_steps=1200
          )
 
+register(id='VSSATKVSGK-v0',
+         entry_point='rsoccer_gym.vss.env_vss:VSSEnvATKVSGK',
+         max_episode_steps=1200
+         )
+
 register(id='SSLGoToBall-v0',
          entry_point='rsoccer_gym.ssl.ssl_go_to_ball:SSLGoToBallEnv',
          kwargs={'field_type': 2, 'n_robots_yellow': 6},
